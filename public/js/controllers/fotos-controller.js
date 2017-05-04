364a4fe204f232eb26ada7a3edcda3e92e1c03b8
baltazarparra <baltazarparra@outlook.com>
2017-05-03 21:56:55 -0300

    ajustes de front

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,9 @@
 angular.module('Galeria').controller('FotosController', function($scope, $http) {
 
 	$scope.fotos = [];
+	$scope.filtro = '';
 
-	$http.get('https://api.pokemontcg.io/v1/cards?series=base&pageSize=12')
+	$http.get('https://api.pokemontcg.io/v1/cards?types=fire')
 	.success(function(retorno) {
 		var imgArr = [];
 		imgArr.push(retorno.cards);
